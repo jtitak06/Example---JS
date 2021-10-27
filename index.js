a642b5124933fc50637bfe89a872c03fe9b10d0f
Jeff Titak <jtitak06@yahoo.com>
2021-10-27 15:13:58 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const firstName = "Jeff";
+let lastName = "Titak";
+var age = 34;
+let city = "Hobart";
+let favoritePasstime = "disc golf";
+
+let fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+
+let myStory = "Hello, my name is " + fullName + ". " + "I am from " + city + ". " + "My favorite passtime is " + favoritePastime + ".";
+console.log(myStory);
